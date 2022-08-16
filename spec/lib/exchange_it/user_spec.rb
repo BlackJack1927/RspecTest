d88071c9e857810ee76a904d79b853ccbc56579a
BlackJack1927 <evgeniy_katkalo@ukr.net>
2022-08-16 17:22:33 +0300

    meta data, focus, fast

--- spec/lib/exchange_it/user_spec.rb
+++ spec/lib/exchange_it/user_spec.rb
@@ -1,24 +1,24 @@
 # frozen_string_literal: true
 
 RSpec.describe ExchangeIt::User do
-  let(:user) { described_class.new('john', 'doe') }
-  let(:user_no_name) { described_class.new(nil, 'doe') }
+  let(:user) { |ex| described_class.new(ex.metadata[:name] || 'john', 'doe') }
+  # let(:user_no_name) { described_class.new(nil, 'doe') } метаданніе заменили метод без имени
   let(:user_no_surname) { described_class.new('john', nil) } # если в заголовке описан класс и модкль то можно его не візівать) а
 
   it 'return name' do
     expect(user.name).to eq('john')
   end
 
-  it 'return name as string' do
-    expect(user_no_name.name).to be_an_instance_of(String)
+  it 'return name as string', name: nil do
+    expect(user.name).to be_an_instance_of(String)
   end
 
   it 'return surname' do
     expect(user.surname).to eq('doe')
   end
 
-  it 'return surname as string' do
-    expect(user.name).to be_an_instance_of(String)
+  it 'return surname as string', surname: nil do
+    expect(user_no_surname.surname).to be_an_instance_of(String)
   end
 
   specify '#account' do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,10 @@
 require_relative '../lib/exchange_it'
 
 RSpec.configure do |config|
-  config.define_derived_metadata(file_path: /exchange_it\/utils/) do |meta|
-     meta[:fast] = true
-  end
+  #config.define_derived_metadata(file_path: /exchange_it\/utils/) do |meta|
+  #ставим метки на все тесті папки utils
+    # meta[:fast] = true
+  #end
+
+   # config.filter_run_excluding :win_only unless RUBY_PLATFORM.match?(/jruby/i) - фильтрация
 end
